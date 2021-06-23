ec52dfd0b66a6051e44a48d5a0543bdbcceec641
Igor Vieira de Cerqueira <40477878+vieiraigor8787@users.noreply.github.com>
2021-06-23 20:39:48 -0300

    pt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,7 +36,7 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert('Room does not exists.')
+      alert('Sala não existe.')
       return;
     }
 
