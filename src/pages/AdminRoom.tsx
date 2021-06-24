0103a4634ede6411abfe75d9d9df5c2d4a217272
Igor Vieira de Cerqueira <40477878+vieiraigor8787@users.noreply.github.com>
2021-06-23 23:01:31 -0300

    end room funcionability

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "./../components/RoomCode";
@@ -20,10 +20,19 @@ type RoomParams = {
 
 export function AdminRoom() {
   // const { user } = useAuth();
+  const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id
 
   const { title, questions } = useRoom(roomId)
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date()
+    })
+
+    history.push('/')
+  }
 
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
@@ -38,7 +47,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
           <RoomCode code={roomId} />
-          <Button isOutlined>Encerrar sala</Button>
+          <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>
         </div>
       </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,6 +40,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Sala já foi encerrada.')
+      return;
+    }
+
     history.push(`rooms/${roomCode}`);
   }
 
